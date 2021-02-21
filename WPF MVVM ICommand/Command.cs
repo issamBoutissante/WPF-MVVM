ec2831eb360d6172d505a79841edcb421cbc1615
issamBoutissante <boutissante.issam.dev@gmail.com>
2021-02-21 19:08:38 +0100

    done

---
+++ WPF MVVM ICommand/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPF_MVVM_ICommand
+{
+    public class Command : ICommand
+    {
+        Action excecuteMethod;
+        
+        public Command(Action excecuteMethod)
+        {
+            this.excecuteMethod = excecuteMethod;
+        }
+        //this is the implemented method
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            excecuteMethod();
+        }
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ WPF MVVM ICommand/ViewModal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF_MVVM_ICommand
+{
+    class ViewModal:INotifyPropertyChanged
+    {
+        private int n1;
+
+        public int N1
+        {
+            get { return n1; }
+            set { if (n1 != value)
+                {
+                    n1 = value;
+                    onPropertyChanged("N1");
+                    onPropertyChanged("Result");
+                }
+            }
+        }
+        private int n2;
+
+        public int N2
+        {
+            get { return n2; }
+            set { if (n2 != value)
+                {
+                    n2 = value; 
+                    onPropertyChanged("N2");
+                    onPropertyChanged("Result");
+                }
+            }
+        }
+        private int result;
+
+        public int Result
+        {
+            get { return N1+N2; }
+            set { if (result != value) {
+                    result = value;
+                    onPropertyChanged("result");
+                };
+            }
+        }
+
+        public ICommand SayHiCommand { get; set; }
+        public ViewModal()
+        {
+            this.SayHiCommand = new Command(() => MessageBox.Show("Hello Issam"));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void onPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
